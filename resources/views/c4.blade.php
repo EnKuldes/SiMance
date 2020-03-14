fd847f288e9094e8949560e9422564a41d805529
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-14 22:41:49 +0700

    c4 view

---
+++ resources/views/c4.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/index')
+
+@section('title', 'C4')
+
+@section('liblary')
+@endsection
+
+@section('extra-liblary')
+@endsection
+
+@section('script')
+@endsection
+
+@section('content')
+Home C4
+@endsection
