a3bfa3963727eba7c25aaeb2c3a272df5602081c
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-14 23:14:51 +0700

    add lib date picker

--- resources/views/c4.blade.php
+++ resources/views/c4.blade.php
@@ -3,9 +3,12 @@
 @section('title', 'C4')
 
 @section('liblary')
+<script src="assets/js/plugins/ui/moment/moment.min.js"></script>
+<script src="assets/js/plugins/pickers/daterangepicker.js"></script>
 @endsection
 
 @section('extra-liblary')
+<script src="assets/js/demo_pages/picker_date.js"></script>
 @endsection
 
 @section('script')
