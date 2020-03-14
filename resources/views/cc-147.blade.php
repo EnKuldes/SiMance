a59081fb775a48108820e872eb4344606afc4c5d
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-14 22:36:58 +0700

    view 147

---
+++ resources/views/cc-147.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/index')
+
+@section('title', 'CC 147')
+
+@section('liblary')
+@endsection
+
+@section('extra-liblary')
+@endsection
+
+@section('script')
+@endsection
+
+@section('content')
+Home CC 147
+@endsection
