baa59888d049510e34c217e4c81d850973be04ba
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-14 22:37:05 +0700

    dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/index')
+
+@section('title', 'Dashboard')
+
+@section('liblary')
+@endsection
+
+@section('extra-liblary')
+@endsection
+
+@section('script')
+@endsection
+
+@section('content')
+Dashboard
+@endsection
