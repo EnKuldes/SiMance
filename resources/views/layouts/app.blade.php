e1ddecf050fbef4b59784460bfe910871bf2c91b
Farhan En Muzaki <fmuzaki12@gmail.com>
2020-03-15 19:02:12 +0700

    - Merge Conflict

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,14 +25,17 @@
   <script src="{{ asset('assets/js/main/jquery.min.js') }}"></script>
   <script src="{{ asset('assets/js/main/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('assets/js/plugins/loaders/blockui.min.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins/notifications/pnotify.min.js') }}"></script>
+  <script src="assets/js/plugins/notifications/pnotify.min.js"></script>
+  <script src="assets/js/plugins/buttons/spin.min.js"></script>
+  <script src="assets/js/plugins/buttons/ladda.min.js"></script>
   <!-- /core JS files -->
 
   <!-- Theme JS files -->
   @yield('liblary')
 
   <script src="{{ asset('assets/js/app.js') }}"></script>
-  {{-- <script src="assets/js/demo_pages/extra_pnotify.js"></script> --}}
+  <script src="assets/js/demo_pages/extra_pnotify.js"></script>
+  <script src="assets/js/demo_pages/components_buttons.js"></script>
   @yield('extra-liblary')
   <!-- /theme JS files -->
 </head>
@@ -122,7 +125,14 @@
     <!-- Main content -->
     <div class="content-wrapper">
 
-      {{-- <button type="button" class="btn btn-light form-control" id="pnotify-styled-left">Launch <i class="icon-play3 ml-2"></i></button> --}}
+      <button type="button" class="btn btn-light form-control" id="pnotify-styled-left">Launch <i class="icon-play3 ml-2"></i></button>
+
+      <button type="button" class="btn btn-light btn-ladda btn-ladda-spinner" data-style="expand-left" data-spinner-color="#333" data-spinner-size="20">
+        <span class="ladda-label">Spinner</span>
+      </button>
+      <button type="button" class="btn bg-teal btn-ladda btn-ladda-progress" data-style="expand-left" data-spinner-size="20">
+        <span class="ladda-label">Spinner + Progress</span>
+      </button>
       <!-- Content area -->
       <div class="content">
         @yield('content')
