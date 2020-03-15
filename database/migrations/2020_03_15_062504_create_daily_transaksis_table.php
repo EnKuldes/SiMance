525d1df969d4b9e139ff0f32f1c2c1a0932e8843
Farhan En Muzaki <fmuzaki12@gmail.com>
2020-03-15 14:36:23 +0700

    - Pembaharuan Seeders Formulasi - Pembuatan Migration dan Model Daily Transaksi

---
+++ database/migrations/2020_03_15_062504_create_daily_transaksis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailyTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('id_layanan');
+            $table->tinyInteger('id_parameter');
+            $table->tinyInteger('id_formulasi');
+            $table->bigInteger('nilai');
+            $table->bigInteger('tanggal');
+            $table->string('user_input');
+            # created_at sebagai tanggal nya
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_transaksis');
+    }
+}
