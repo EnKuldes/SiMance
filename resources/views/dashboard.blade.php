bfbab8271deec3b92ece58f7b4d2f5cf495f133d
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-18 21:19:13 +0700

    minor-fix design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -266,7 +266,7 @@
                     {{ $kpiObject[0]->target  ?? 0}} | Bobot {{ $kpiObject[0]->bobot  ?? 0}}</span>
                 </div>
                 <div class="progress" style="height: 1.5rem;">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_service_level  ?? 0) > $kpiObject[0]->target ? "success" : "danger" }}" style="width: <?=$width_progressbar_sl ?? 0?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_service_level  ?? 0) > ($kpiObject[0]->target  ?? 0) ? "success" : "danger" }}" style="width: <?=$width_progressbar_sl ?? 0?>%">
                     <span>{{ round($target_service_level  ?? 0) }}% Complete</span>
                   </div>
                 </div>
@@ -276,7 +276,7 @@
                 <div class="d-flex align-items-center mb-1">FCR <span class="text-muted ml-auto">Target
                     {{ $kpiObject[1]->target ?? 0 }} | Bobot {{ $kpiObject[1]->bobot ?? 0 }}</span></div>
                 <div class="progress" style="height: 1.5rem;">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_fcr  ?? 0) > $kpiObject[1]->target ? "success" : "danger" }}" style="width: <?=$width_progressbar_fcr ?? 0 ?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_fcr  ?? 0) > ($kpiObject[1]->target  ?? 0) ? "success" : "danger" }}" style="width: <?=$width_progressbar_fcr ?? 0 ?>%">
                     <span>{{ round($target_fcr ?? 0 ) }}% Complete</span>
                   </div>
                 </div>
@@ -286,7 +286,7 @@
                 <div class="d-flex align-items-center mb-1">Rasio Sales <span class="text-muted ml-auto">Target
                     {{ $kpiObject[2]->target ?? 0 }} | Bobot {{ $kpiObject[2]->bobot ?? 0 }}</span></div>
                 <div class="progress" style="height: 1.5rem;">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_rasio_sales  ?? 0) > $kpiObject[2]->target ? "info" : "danger" }}" style="width: <?=$width_progressbar_rs ?? 0 ?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_rasio_sales  ?? 0) > ($kpiObject[2]->target  ?? 0) ? "info" : "danger" }}" style="width: <?=$width_progressbar_rs ?? 0 ?>%">
                     <span>{{ round($target_rasio_sales ?? 0 ) }}% Complete</span>
                   </div>
                 </div>
@@ -296,7 +296,7 @@
                 <div class="d-flex align-items-center mb-1">CES (by customer) <span class="text-muted ml-auto">Target
                     {{ $kpiObject[3]->target ?? 0 }} | Bobot {{ $kpiObject[3]->bobot ?? 0 }}</span></div>
                 <div class="progress" style="height: 1.5rem;">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_ces  ?? 0) > $kpiObject[3]->target ? "info" : "danger" }}" style="width: <?=$width_progressbar_ces ?? 0 ?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_ces  ?? 0) > ($kpiObject[3]->target  ?? 0) ? "info" : "danger" }}" style="width: <?=$width_progressbar_ces ?? 0 ?>%">
                     <span>{{ round($target_ces ?? 0 ) }}% Complete</span>
                   </div>
                 </div>
@@ -307,11 +307,11 @@
                     {{ $kpiObject[4]->target ?? 0 }} | Bobot
                     {{ $kpiObject[4]->bobot ?? 0 }}</span></div>
                 <div class="progress" style="height: 1.5rem;">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round(100-$target_quality_layanan  ?? 0) < $revert_target ? "info" : "info" }}" style="width: <?= ((100-$target_quality_layanan) ?? 0) == 100 ? 0 : 100-$target_quality_layanan; ?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round(100-$target_quality_layanan  ?? 0) < $revert_target ? "info" : "info" }}" style="width: <?= ((100-$target_quality_layanan) ?? 0) == 100 ? 0 : ((100-$target_quality_layanan) ?? 0); ?>%">
                     <span>{{ round(100 - $target_quality_layanan ?? 0 ) }}% OK</span>
                   </div>
 
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_quality_layanan  ?? 0) <= $kpiObject[4]->target ? "success" : "danger" }}" style="width: <?=$target_quality_layanan ?? 0 ?>%">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-{{ round($target_quality_layanan  ?? 0) <= ($kpiObject[4]->target  ?? 0) ? "success" : "danger" }}" style="width: <?=$target_quality_layanan ?? 0 ?>%">
                     <span>{{ round($target_quality_layanan ?? 0 ) }}% NOK</span>
                   </div>
                 </div>
