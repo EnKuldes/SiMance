5fc421b7f42db4650b3f271d7ceb44f13ba8f1d8
Ridlafalah <fadelridlafalah@gmail.com>
2020-03-20 22:48:11 +0700

    preload card area

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -166,7 +166,7 @@
     <div class="content-wrapper">
       <!-- Content area -->
       <div class="content">
-        {{-- <button type="button" class="btn btn-primary" id="block-page">Block the whole page</button> --}}
+        <button type="button" class="btn btn-primary legitRipple" id="block-card">Block this card</button>
         @yield('content')
       </div>
       <!-- /content area -->
