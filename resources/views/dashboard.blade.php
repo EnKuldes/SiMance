8adcd59619b73dd90f295cd756913429142283e4
Farhan En Muzaki <fmuzaki12@gmail.com>
2020-11-13 22:43:42 +0700

    Fix Dashboard Information Total Transkasi Parameter Rasio Sales Layanan Digital Media

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -146,7 +146,7 @@
             content += '<td>';
             if (data[i]['paramater_id'] == 3 || data[i]['paramater_id'] == 8) {
               content += '<p class="font-weight-bold font-size-sm text-center mb-0 mt-3 '
-              content += 'text-success">'+new Intl.NumberFormat().format( parseFloat(data[i]['realisasi_per_formulasi'][0]['formulasi_total']) + parseFloat(data[i]['realisasi_per_formulasi'][1]['formulasi_total']) )+'</p>';
+              content += 'text-success">'+new Intl.NumberFormat().format( parseFloat(data[i]['realisasi_per_formulasi'][0]['formulasi_total']) + parseFloat(data[i]['realisasi_per_formulasi'][1]['formulasi_total']) + parseFloat(data[i]['realisasi_per_formulasi'][3]['formulasi_total']) + parseFloat(data[i]['realisasi_per_formulasi'][4]['formulasi_total']) )+'</p>';
               content += '<p class="font-weight-bold font-size-sm text-center mb-0 text-success">'+new Intl.NumberFormat().format(data[i]['realisasi_per_formulasi'][2]['formulasi_total'])+'</p>';
             }
             else{
